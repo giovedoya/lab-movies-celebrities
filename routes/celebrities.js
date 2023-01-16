7f02e5257bcf2efd74fdf5c4093919d2da8fe402
Giovanni Bedoya <giovedoya@gmail.com>
2023-01-16 21:46:06 +0100

    'Iteration 3: Adding New Celebrities'

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
+
+/* GET form celebrities*/
+/* ROUTE celebrities/create*/
+router.get('/celebrities/create', async (req, res, next) => {
+    res.render('./celebrities/new-celebrity')
+})
+
+/* POST create celebrities*/
+/* ROUTE celebrities/create*/
+router.post('/celebrities/create', async (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    try {
+        await Celebrity.create({ name, occupation, catchPhrase});
+        res.redirect('/celebrities')
+    } catch (error) {
+        
+    }
+})
+
 
 
 
